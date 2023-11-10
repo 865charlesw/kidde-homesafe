f3caa7d89523d6439bc1588517f7ef3d489677ef
Your Name <you@example.com>
2023-11-10 13:12:03 -0700

    Optimized get_data, added error for duplicate IDs

--- kidde_homesafe.py
+++ kidde_homesafe.py
@@ -10,7 +10,16 @@ _API_PREFIX = "https://api.homesafe.kidde.com/api/v4"
 
 def _dict_by_ids(items: list[dict]) -> dict[int, dict]:
     """Create a dictionary from a list of items, keyed by ID."""
-    return {item["id"]: item for item in items}
+    result = {item["id"]: item for item in items}
+    if len(result) != len(items):
+        ids = set()
+        duplicates = []
+        for item in items:
+            if item["id"] in ids:
+                duplicates.append(item)
+            ids.add(item["id"])
+        raise ValueError(f"Duplicate IDs: {duplicates}")
+    return result
 
 
 class KiddeCommand(StrEnum):
@@ -107,15 +116,19 @@ class KiddeClient:
         """
         location_list = await self._request("location")
         locations = _dict_by_ids(location_list)
-        devices = {} if get_devices else None
-        events = {} if get_events else None
-        for location_id in locations:
-            if get_devices:
-                device_list = await self._request(f"location/{location_id}/device")
-                devices.update(_dict_by_ids(device_list))
-            if get_events:
-                events_result = await self._request(f"location/{location_id}/event")
-                events.update(_dict_by_ids(events_result["events"]))
+        devices = events = None
+        if get_devices:
+            devices_list = []
+            for location_id in locations:
+                location_devices = await self._request(f"location/{location_id}/device")
+                devices_list.extend(location_devices)
+            devices = _dict_by_ids(devices_list)
+        if get_events:
+            events_list = []
+            for location_id in locations:
+                location_events = await self._request(f"location/{location_id}/event")
+                events_list.extend(location_events["events"])
+            events = _dict_by_ids(events_list)
         return KiddeDataset(locations, devices, events)
 
     async def device_command(
